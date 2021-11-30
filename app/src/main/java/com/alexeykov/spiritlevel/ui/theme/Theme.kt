4e323323b6a6bd38f60cdf518cd4a786f7442bfc
Alexeykov <alexeykov.soft@gmail.com>
2021-11-30 12:15:16 +0300

    Worked update

--- app/src/main/java/com/alexeykov/spiritlevel/ui/theme/Theme.kt
+++ app/src/main/java/com/alexeykov/spiritlevel/ui/theme/Theme.kt
@@ -13,9 +13,9 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Blue200,
+    primary = Blue500,
     primaryVariant = Blue700,
-    secondary = Teal200
+    secondary = Blue200
 
     /* Other default colors to override
     background = Color.White,
@@ -30,7 +30,7 @@ private val LightColorPalette = lightColors(
 @Composable
 fun SpiritLevelTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable() () -> Unit
+    content: @Composable () -> Unit
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
